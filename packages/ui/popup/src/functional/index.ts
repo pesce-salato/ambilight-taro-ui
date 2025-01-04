21ceda31398b60da9693ad60379408d7cac83706
mayfly <59794462+x-glorious@users.noreply.github.com>
2025-01-04 17:27:07 +0800

    feat(popup): add functional call to show

---
+++ packages/ui/popup/src/functional/index.ts
@@ -0,0 +1,66 @@
+import { safeRenderToController } from '@ambilight-taro/page-view'
+import { AlPopup } from '../component'
+import { type AlPopupProps } from '../component/type'
+
+export interface AlPopupFunctionalShowSetting
+  extends Omit<AlPopupProps, 'visible' | '_functionCall'> {}
+
+export interface AlPopupStatic {
+  show: (
+    setting: AlPopupFunctionalShowSetting,
+    id?: string,
+  ) => {
+    close: () => void
+    changeSetting: (newSetting: AlPopupFunctionalShowSetting) => void
+  }
+}
+
+export const functionalWrapper = (component: typeof AlPopup) => {
+  const wrappedComponent = component as typeof AlPopup & AlPopupStatic
+
+  wrappedComponent.show = (setting, targetId) => {
+    let latestSetting = setting
+
+    // eslint-disable-next-line unicorn/consistent-function-scoping
+    let deleteMyself = () => {
+      // default
+    }
+
+    const controller = safeRenderToController({
+      component: AlPopup,
+      targetId,
+      props: {
+        ...setting,
+        _functionCall: true,
+        visible: true,
+        onHide: () => {
+          latestSetting.onHide?.()
+          deleteMyself()
+        },
+      },
+    })
+
+    deleteMyself = controller.remove
+
+    return {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      changeSetting: (newSetting: any = {}) => {
+        latestSetting = {
+          ...latestSetting,
+          ...newSetting,
+        }
+        // forbidden to set the system control props
+        const { _functionCall, visible, onHide, ...others } =
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          latestSetting as any
+
+        controller.changeProps(others)
+      },
+      close: () => {
+        controller.changeProps({ visible: false })
+      },
+    }
+  }
+
+  return wrappedComponent
+}
--- packages/ui/popup/src/index.ts
+++ packages/ui/popup/src/index.ts
@@ -1,2 +1,7 @@
-export * from './component'
+import { AlPopup as _AlPopup } from './component'
+import { functionalWrapper } from './functional'
+
+export const AlPopup = functionalWrapper(_AlPopup)
+
+export { AlPopupFunctionalShowSetting, AlPopupStatic } from './functional'
 export * from './component/type'
