8dc4e0baa6ab1c00f0689f9b586bdbbf4c07c983
cuckoo <ordinary_person@outlook.com>
2025-03-19 22:40:31 +0800

    feat(demo): card component 支持 desc

--- packages/demo/src/components/card/index.tsx
+++ packages/demo/src/components/card/index.tsx
@@ -13,6 +13,7 @@ export interface CardProps {
 export interface CardItemProps {
   children?: ReactNode
   label?: ReactNode
+  desc?: ReactNode
 }
 
 export const Card = (props: CardProps) => {
@@ -27,7 +28,12 @@ export const Card = (props: CardProps) => {
 export const CardItem = (props: CardItemProps) => {
   return (
     <View className={root.hierarchies('item').className}>
-      <View className={root.hierarchies(['item', 'label']).className}>{props.label}</View>
+      <View className={root.hierarchies(['item', 'label']).className}>
+        {props.label}
+        {props.desc && (
+          <View className={root.hierarchies(['item', 'desc']).className}>{props.desc}</View>
+        )}
+      </View>
       <View className={root.hierarchies(['item', 'content']).className}>{props.children}</View>
     </View>
   )
